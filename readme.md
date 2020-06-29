6ab7ea9e119ffde31d00e774d561ad9a764d1e83
admin <rong@agile-solutions.ch>
2020-06-29 17:15:57 +0200

    refinements

--- readme.md
+++ readme.md
@@ -1,5 +1,12 @@
 # study links
 
+Just a test thing
+
+1. cucumber
+2. Spring web mvc testing with mockmvc, hamcrest
+3. zalando problem
+4. controller advice
+
 * [streams learning](https://winterbe.com/posts/2014/07/31/java8-stream-tutorial-examples/)
 * [primitives](https://www.baeldung.com/java-primitives)
 * [Testing in springboot](https://www.baeldung.com/spring-boot-testing)
---
+++ src/main/java/com/mycomp/test/problem/ExceptionTranslator.java
@@ -0,0 +1,26 @@
+package com.mycomp.test.problem;
+
+import org.apache.tomcat.util.http.HeaderUtil;
+import org.springframework.beans.factory.parsing.Problem;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.zalando.problem.spring.web.advice.ProblemHandling;
+import org.zalando.problem.spring.web.advice.security.SecurityAdviceTrait;
+
+/**
+ * Controller advice to translate the server side exceptions to client-friendly json structures.
+ * The error response follows RFC7807 - Problem Details for HTTP APIs (https://tools.ietf.org/html/rfc7807).
+ */
+@ControllerAdvice
+public class ExceptionTranslator implements ProblemHandling, SecurityAdviceTrait {
+	
+
+    @ExceptionHandler
+    public ResponseEntity<Problem> handleBadRequestAlertException(BadRequestAlertException ex, NativeWebRequest request) {
+        return create(ex, request, HeaderUtil.createFailureAlert(applicationName, true, ex.getEntityName(), ex.getErrorKey(), ex.getMessage()));
+    }
+
+
+}
